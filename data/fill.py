d2eebb9bcf4631952aeae469594ae30e33a6c75a
elias <eliasvelardez38@gmail.com>
2021-07-13 17:36:28 -0300

    removed some comments

--- data/fill.py
+++ data/fill.py
@@ -3,25 +3,19 @@ from psycopg2 import connect, Error
 from random import randint
 
 with open('./clients.json') as f:
-
-    # use load() rather than loads() for JSON files
     clients = json.load(f)
 
 with open('./credit_requests.json') as f2:
-
-    # use load() rather than loads() for JSON files
     credit_requests = json.load(f2)
 
 
 try:
-    # declare a new PostgreSQL connection object
     conn = connect(
         dbname = "limadb",
         user = "limauser",
         host = "127.0.0.1",
         password = "limapassword",
         port = "5432",
-        # attempt to connect for 3 seconds then raise exception
         connect_timeout = 5
     )
 
@@ -39,7 +33,7 @@ if cur != None:
     try:
         risks = ['Bueno', 'Regular', 'Malo']
         statuses = ['Pendiente', 'Cancelada', 'Aprobada']
-        
+
         for c in clients:
             sql_string = f'INSERT INTO credits_client(first_name, last_name, sbs_debt, sentinel_risk_score) VALUES(\'{c["first_name"]}\', \'{c["last_name"]}\', {c["sbs_debt"]}, \'{risks[c["sentinel_risk_score"]]}\')'
             cur.execute( sql_string )
@@ -58,6 +52,5 @@ if cur != None:
         print("\nexecute_sql() error:", error)
         conn.rollback()
 
-    # close the cursor and connection
     cur.close()
     conn.close()
